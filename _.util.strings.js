e4a3a591479795ec870c5638539dcdad5381aa3e
Refael Ackermann <refael@empeeric.com>
2013-12-17 10:58:29 +0200

    Two new string helpers

--- _.util.strings.js
+++ _.util.strings.js
@@ -2,7 +2,7 @@
 // (c) 2013 Michael Fogus, DocumentCloud and Investigative Reporters & Editors
 // lodash-contrib may be freely distributed under the MIT license.
 
-(function(root) {
+(function (root) {
 
   // Baseline setup
   // --------------
@@ -18,31 +18,45 @@
 
   _.mixin({
     // Explodes a string into an array of chars
-    explode: function(s) {
+    explode: function (s) {
       return s.split('');
     },
 
     // Implodes and array of chars into a string
-    implode: function(a) {
+    implode: function (a) {
       return a.join('');
     },
 
     // Converts a string to camel case
-    camelCase : function( string ){
-      return  string.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+    camelCase: function (string) {
+      return  string.replace(/-([a-z])/g, function (g) {
+        return g[1].toUpperCase();
+      });
     },
 
     // Converts camel case to dashed (opposite of _.camelCase)
-    toDash : function( string ){
-      string = string.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();});
+    toDash: function (string) {
+      string = string.replace(/([A-Z])/g, function ($1) {
+        return "-" + $1.toLowerCase();
+      });
       // remove first dash
-      return  ( string.charAt( 0 ) == '-' ) ? string.substr(1) : string;
+      return  ( string.charAt(0) == '-' ) ? string.substr(1) : string;
     },
 
     // Reports whether a string contains a search string.
-    strContains: function(str, search) {
+    strContains: function (str, search) {
       if (typeof str != 'string') throw new TypeError;
       return (str.indexOf(search) != -1);
+    },
+
+    // Reports whether a string contains a search string.
+    capitalize: function capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    },
+
+    // Slugify a string. Makes lowercase, and converts dots and spaces to dashes.
+    slugify: function (urlString) {
+      return urlString.replace(/ /g, '-').replace(/\./, '').toLowerCase();
     }
 
   });
