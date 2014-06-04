19a812c4088812e91741cbcc3b47f1549d64e511
Refael Ackermann <refael@empeeric.com>
2014-06-04 18:04:05 +0300

    cwd is a bi**h

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var fs = require('fs');
 var vm = require('vm');
+var path = require('path');
 var _ = require('lodash').runInContext();
 
 var isAddOnRegEx = _.bindAll(/^_\..+/, 'test');
-var context = vm.createContext({_:_});
+var context = vm.createContext({_: _});
 var rightCurryVm = _.partialRight(vm.runInNewContext, context);
-function simpleReadFile(filename) { return fs.readFileSync(filename); }
+function simpleReadFile(filename) {
+  return fs.readFileSync(path.join(__dirname, filename));
+}
 _(fs.readdirSync(__dirname)).filter(isAddOnRegEx.test).map(simpleReadFile).each(function (code) {
   rightCurryVm(code);
 });
